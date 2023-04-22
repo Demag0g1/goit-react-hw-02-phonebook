c47b258760fd66ab521fa4626b65fe165e63edc6
Demag0g1 <konievanatol@gmail.com>
2023-04-22 21:32:33 +0300

    up first init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
-import { Component } from "react"
-
-export class App extends Component  {
+export const App = () => {
   return (
-   
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
   );
 };
